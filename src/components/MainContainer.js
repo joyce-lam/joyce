5b69ef7f1e69cf32d5bd09bf100bfc99088952ad
Joyce <joycelamsaungan@gmail.com>
2018-03-27 20:23:45 -0700

    deleting unused code

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -2,7 +2,6 @@ import React from "react";
 import Banner from "./Banner";
 import AboutMe from "./AboutMe";
 import BodyContainer from "./BodyContainer";
-import images from "../images.js";
 
 export default class MainContainer extends React.Component {
 
@@ -10,10 +9,7 @@ export default class MainContainer extends React.Component {
 		super(props);
 		this.state = {
 			github: `https://github.com/joyce-lam`,
-			linkedin: `https://www.linkedin.com/in/joycesnlam/`,			
-			ghlogo: images[1].github,
-			lnlogo: images[2].linkedin,
-			map: images[0].worldmap
+			linkedin: `https://www.linkedin.com/in/joycesnlam/`
 		};
 	}
 
@@ -30,9 +26,6 @@ export default class MainContainer extends React.Component {
 	            	<AboutMe 
 	            	githublink={this.state.github}
 	            	linkedinlink={this.state.linkedin}
-	            	onClick={this.handleClick}
-	            	ghlogo={this.state.ghlogo}
-	            	lnlogo={this.state.lnlogo}
 	            	/>
 	            	<BodyContainer />
 	            </div>
