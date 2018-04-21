2a8c6e653e80986a21f0654a05224bbfbbf13f23
Joyce <joycelamsaungan@gmail.com>
2018-04-21 14:41:48 -0700

    fixing formatting

--- src/components/Experience/Experience.js
+++ src/components/Experience/Experience.js
@@ -8,7 +8,7 @@ const Experience = () => (
 			<p>2018</p>
 			<p>Software Engineer</p>
 			<p>Seat Swap Project</p>
-			<div>
+			<div className="content">
 				<p>A real-time marketplace for ticket swaps between season ticket subscribers.</p>
 				<p>Built with Node.js + Express backend and React frontend.</p>
 				<p>Authenticated users using OAuth 2.0 with passport.js authentication middleware via social networks pke Google and Facebook.</p>
@@ -19,7 +19,7 @@ const Experience = () => (
 			<p>2012 - 2017</p>
 			<p>Clinical Dietitian</p>
 			<p>Hospital Authority (Queen Mary Hospital), Hong Kong</p>
-			<div>
+			<div className="content">
 				<p>Depvered group education and individual nutritional counsepng in Engpsh, Cantonese, and Mandarin to diabetic patients.</p>
 				<p>Provided nutrition screening and education to seniors.</p>
 				<p>Provided medical nutrition therapy to patients in general medical, orthopaedics, and obstetrics and gynaecology wards.</p>
@@ -30,11 +30,11 @@ const Experience = () => (
 			<p>2014 - 2017</p>
 			<p>Guest Lecturer</p>
 			<p>Hong Kong</p>
-			<div>
-				<p>Diabetes Education and Management Course organized by Queen Mary Hospital and The University of Hong Kong (2014-2017).</p>
-				<p>L series of A-Z symposium organized by the Hong Kong Society of Endocrinology, Metabopsm, and Reproduction (2016 Apr 21).</p>
-				<p>Update of Diabetes Management organized by Queen Mary Hospital and The University of Hong Kong (2015 Oct 31).</p>
-				<p>Post-graduate Study Day for cpnicians organized by the Department of Obstetrics and Gynaecology of The University of Hong Kong (2015 Apr 1).</p>
+			<div className="content">
+				<p>1. Diabetes Education and Management Course organized by Queen Mary Hospital and The University of Hong Kong (2014-2017).</p>
+				<p>2. L series of A-Z symposium organized by the Hong Kong Society of Endocrinology, Metabopsm, and Reproduction (2016 Apr 21).</p>
+				<p>3. Update of Diabetes Management organized by Queen Mary Hospital and The University of Hong Kong (2015 Oct 31).</p>
+				<p>4. Post-graduate Study Day for cpnicians organized by the Department of Obstetrics and Gynaecology of The University of Hong Kong (2015 Apr 1).</p>
 			</div>
 		</div>
 	</div>
